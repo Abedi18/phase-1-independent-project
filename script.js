84d4ab41a61e9b55b7c613fcccb3c1992b6e0cd9
Abednego <abedikroos15@gmail.com>
2023-04-08 11:52:38 +0300

    Getting HTML elements by their ID

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-
+// Get the HTML elements by their ID
+let result = document.getElementById("result");
+let searchBtn = document.getElementById("search-btn");
+let cityRef = document.getElementById("city");
